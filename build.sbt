1778ffee0cd5e4c16e88f8ecdd144b6920b99297
Peteerrr <po21158@gmail.com>
2016-08-25 13:24:00 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ZepTest"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala-2.11/zeptest/Main.scala
@@ -0,0 +1,10 @@
+package zeptest
+
+/**
+  * Created by am_dev on 8/25/16.
+  */
+object Main extends App {
+  println("ello")
+
+  def giveMeInt: Int = 5
+}
